4dca62ac2e43469819f8fef8f3e53a61eab0178a
Yesil <ahmetysl69@gmail.com>
2025-05-20 15:40:11 +0200

    login4

--- routes/users.js
+++ routes/users.js
@@ -12,20 +12,25 @@ const { checkBody } = require("../modules/checkBody");
 const uid2 = require("uid2");
 const bcrypt = require("bcrypt");
 
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+const uniqid = require("uniqid");
+
 router.post("/signup", (req, res) => {
-  if (!checkBody(req.body, ["username", "email", "password"])) {
+  if (!checkBody(req.body, ["email", "password"])) {
     res.json({ result: false, error: "Missing or empty fields" });
     return;
   }
 
   User.findOne({
-    $or: [{ username: req.body.username }, { email: req.body.email }],
+    $or: [{ email: req.body.email }],
   }).then((data) => {
     if (data === null) {
       const hash = bcrypt.hashSync(req.body.password, 10);
 
       const newUser = new User({
-        username: req.body.username,
+        avatar: null,
+        nickname: null,
         email: req.body.email,
         password: hash,
         token: uid2(32),
@@ -53,21 +58,55 @@ router.post("/signin", (req, res) => {
     }
   });
 });
-router.post("/profile", (req, res) => {
-  if (!checkBody(req.body, ["token", "nickname", "avatar"])) {
+router.put("/profile", (req, res) => {
+  if (
+    !checkBody(req.body, ["token", "nickname"]) ||
+    !req.files ||
+    !req.files.photoFromFront
+  ) {
     res.json({ result: false, error: "Missing or empty fields" });
     return;
   }
+
   User.findOne({ token: req.body.token }).then((user) => {
     if (!user) {
       res.json({ result: false, error: "User not found" });
       return;
     }
-    user.nickname = req.body.nickname;
-    user.avatar = req.body.avatar;
-    user.save().then(() => {
-      res.json({ result: true });
+
+    const photoPath = `./tmp/${uniqid()}.jpg`;
+    req.files.photoFromFront.mv(photoPath, (err) => {
+      if (err) {
+        res.json({ result: false, error: "File upload failed" });
+        return;
+      }
+
+      cloudinary.uploader.upload(photoPath, (error, resultCloudinary) => {
+        fs.unlinkSync(photoPath); 
+
+        if (error) {
+          res.json({ result: false, error: "Cloudinary upload failed" });
+          return;
+        }
+
+        user.nickname = req.body.nickname;
+        user.avatar = resultCloudinary.secure_url;
+        user.save().then(() => {
+          res.json({ result: true, url: resultCloudinary.secure_url });
+        });
+      });
     });
+  });
+});
+
+router.get("/:token", (req, res) => {
+  User.findOne({ token: req.params.token }).then((user) => {
+    if (user) {
+      console.log(user);
+      res.json({ result: true, user });
+    } else {
+      res.json({ result: false, error: "User not found" });
+    }
   });
 });
 
