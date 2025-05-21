f9db654d933be5b31fa0a690208fd002fe57f043
paulinepasseri <paulinecpasseri@gmail.com>
2025-05-21 16:38:52 +0200

    route user

--- routes/users.js
+++ routes/users.js
@@ -47,7 +47,7 @@ router.post("/signin", (req, res) => {
   }
   User.findOne({ email: req.body.email }).then((data) => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, token: data.token });
+      res.json({ result: true, token: data.token, avatar: data.avatar, nickname: data.nickname });
     } else {
       res.json({ result: false, error: "User not found or wrong password" });
     }
