55c762ec396945a6ca733b028185f0841455bb3c
paulinepasseri <paulinecpasseri@gmail.com>
2025-05-26 12:15:43 +0200

    test

--- app.test.js
+++ app.test.js
@@ -1,10 +1,23 @@
-const request = require('supertest');
-const app = require('./app');
+const request = require("supertest");
+const app = require("./app");
 
-// Exemple
-it('GET /products', async () => {
- const res = await request(app).get('/products');
+// Test route get /games/game/:code
+it("GET /games/game/wzoag", async () => {
+  const res = await request(app).get("/games/game/wzoag");
 
- expect(res.statusCode).toBe(200);
- expect(res.body.stock).toEqual(['iPhone', 'iPad', 'iPod']);
+  expect(res.statusCode).toBe(200);
+  expect(res.body.game).toEqual({
+    _id: "68341642703c35f29513e1a8",
+    status: false,
+    code: "wzoag",
+    title: "Dragon",
+    image:
+      "https://res.cloudinary.com/dxgix5q4e/image/upload/v1747834157/dragon_xabusn.png",
+    genre: "Aventure",
+    nbPlayers: 4,
+    nbScenes: 8,
+    winner: "EZmsJzuRLhSyBhCGRhBt1juy7G7ylsuo",
+    usersId: ["682d9a48b818070dd37cdd60"],
+    __v: 0,
+  });
 });
