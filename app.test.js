0c0bc9977bf7654b36a6e87f2d750f1ac47d3ef7
paulinepasseri <paulinecpasseri@gmail.com>
2025-05-26 12:06:49 +0200

    file app.test

---
+++ app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('./app');
+
+// Exemple
+it('GET /products', async () => {
+ const res = await request(app).get('/products');
+
+ expect(res.statusCode).toBe(200);
+ expect(res.body.stock).toEqual(['iPhone', 'iPad', 'iPod']);
+});
